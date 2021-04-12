75fd1dfa8a6c62403e743d1d532d783d9982f776
Mazin Ghafadgi <mazinghafadgi@Mazins-MacBook-Pro-2.local>
2021-04-12 19:56:39 +0100

    Added more rules

--- src/scala/vodafone/dataqulaity/SQLEngine.scala
+++ src/scala/vodafone/dataqulaity/SQLEngine.scala
@@ -19,17 +19,22 @@ object RuleSet {
           val sql = size(target)
           println(sql)
           sql
-        case "StatisticsRule"                         => function(ruleValue, target, ruleValue)
+        case "StatisticsRule"                         => function(ruleValue, target)
         case "Uniqueness"                             => uniqueness(ruleValue, target)
         case "ProportionMissingInPercentage"          => s"SELECT CAST (SUM (CASE WHEN $ruleValue is NULL THEN 1 ELSE 0 END) as float) / COUNT(*) AS ProportionMissing FROM $sourceName"
 
-        case "ValidateDate"   =>  {
-          val sql = s"SELECT count(to_date($ruleValue, 'yyyy-mm-dd')) from $target"
+        case "DateTimeRule"   =>  {
+          val sql = s"SELECT count(to_date($ruleValue, 'yyyy/mm/dd')) from $target"
           println(sql)
           sql
         }
-        case "ValidateEmailAddress" => {
+        case "EmailRule" => {
           val sql = s"SELECT count(*) from $target where $ruleValue like '%_@__%.__%'"
+          println(sql)
+          sql
+        }
+        case "InvalidEmailRule" => {
+          val sql = s"SELECT count(*) from $target where $ruleValue not like '%_@__%.__%'"
           println(sql)
           sql
         }
@@ -55,7 +60,8 @@ object RuleSet {
 
 
  private def size(target: String)                            = s"SELECT count(*) FROM $target"
- private def function(functionValue: String, target: String, filter: String) = s"SELECT $functionValue FROM $target where $filter"
+ private def function(functionValue: String, target: String) = s"SELECT $functionValue FROM $target"
+ private def functionAndFilter(functionValue: String, target: String, filter: String) = s"SELECT $functionValue FROM $target where $filter"
  private def uniqueness(column: String, target: String)      = s"SELECT count(distinct $column) FROM $target"
  private def default(filter: String, target: String)         = s"SELECT count(*) FROM $target where $filter"
 
