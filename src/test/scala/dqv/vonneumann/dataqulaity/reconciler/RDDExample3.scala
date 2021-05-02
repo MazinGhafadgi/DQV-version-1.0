c9a6ce05694387b35ac921c080854f30435c25d6
Mazin Ghafadgi <mazinghafadgi@Mazins-MacBook-Pro-2.local>
2021-05-02 11:56:01 +0100

    Improved Error handling in RRDExample3

--- src/test/scala/dqv/vonneumann/dataqulaity/reconciler/RDDExample3.scala
+++ src/test/scala/dqv/vonneumann/dataqulaity/reconciler/RDDExample3.scala
@@ -1,52 +1,55 @@
 package dqv.vonneumann.dataqulaity.reconciler
 
-import dqv.vonneumann.dataqulaity.model.ReconcilerModel
 import dqv.vonneumann.dataqulaity.sparksession.SparkSessionFactory
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
+
+case class MetaData(columnName: String, expectedValue: String, typeColumn: String)
+
 object RDDExample3 extends App {
+
+  val metaData = List(MetaData("language", "Java", "string"), MetaData("users_count", "3000", "int"))
 
   implicit val sparkSession = SparkSessionFactory.createSparkSession("local")
   import sparkSession.implicits._
 
   val columns = Seq("language","users_count")
-  val data = Seq(("Java", "20000"), ("Python", "100000"), ("Scala", "3000"))
+  val data = Seq(("Java", 20000), ("Python", 100000), ("Scala", 3000))
   val rddTest = sparkSession.sparkContext.parallelize(data)
-  val dataFrame = rddTest.toDF()
-  dataFrame.show(false)
+  val dataFrame = rddTest.toDF(columns: _*)
 
+  def filterWithTargetColumn(targetColumn: String, targetValue: Int ) = {
+    dataFrame.rdd.mapPartitions {
+      itr => itr.map {
+        row => FilterDFRow.filterWithTargetColumn(row, targetColumn, targetValue)
+      }
+    }
+  }
 
-  val rdd = dataFrame.rdd.mapPartitions(iter => {
-    iter.map(x => FilterDFRow.filter(x) )
-  })
+  val filteredRDD = filterWithTargetColumn("users_count", 3000)
 
-  val badRDD  = rdd.filter(_.isLeft).map(x => x.left.get)
-  val goodRDD = rdd.filter(_.isRight).map(x => x.right.get)
-  val goodSchema = StructType(Array(
-    StructField("goodRow",StringType,true)
-  ))
-  val badSchema = StructType(Array(
-    StructField("badRow",StringType,true)
-  ))
+  val badRDD1 = filteredRDD.filter(_.isLeft).map(_.left.get)
+  val goodRDD1 = filteredRDD.filter(_.isRight).map(_.right.get)
 
-  val fullSchema = StructType(Array(
-    StructField("Lang",StringType,true),
-    StructField("Salary",StringType,true)
+  val errorSchema = StructType(Array(
+    StructField("Row",StringType,true),
+    StructField("error",StringType,true)
   ))
 
+  sparkSession.sqlContext.createDataFrame(badRDD1, errorSchema).show(false)
+  sparkSession.sqlContext.createDataFrame(goodRDD1, dataFrame.schema).show(false)
 
-  sparkSession.sqlContext.createDataFrame(goodRDD, fullSchema).show(false)
-  sparkSession.sqlContext.createDataFrame(badRDD, fullSchema).show(false)
 }
 
 object FilterDFRow {
-  def filter(row: Row): Either[Row, Row] = {
-    if(row.getString(0) == "Java") {
-      Left(Row(s"BadRow ${row.getString(0)}", "No Salary"))
-    } else {
+  def filterWithTargetColumn(row: Row, column: String, expectedValue: Int): Either[Row, Row] = {
+    val colName = row.schema.map(_.name).find(e => e == column).get
+    val extractValue = row.getAs(colName).asInstanceOf[Int]
+    if(extractValue > expectedValue)
+      Left(Row(row.toString(), "Failed validation due to salary is less than 3000"))
+    else
       Right(row)
-    }
   }
 }
 
